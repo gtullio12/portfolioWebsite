cc76afb1728f9e875f18fad8084da6b748e79507
G T <gt@Gs-MacBook-Air.local>
2022-11-28 13:57:49 -0800

    commit

--- inc/sendEmail.php
+++ inc/sendEmail.php
@@ -1,19 +1,20 @@
 ﻿<?php
 
-// Replace this with your own email address
-$siteOwnersEmail = 'gtullio12@protonmail.com';
-
 if($_POST) {
+
+	$email = trim(stripslashes($_POST['contactEmail']));
+   	$subject = trim(stripslashes($_POST['contactSubject']));
+   	$contact_message = trim(stripslashes($_POST['contactMessage']));
 
 	$name = trim(stripslashes($_POST['contactName']));
 	$message .= "Message from: " . $name . "<br />";
 	$message .= "Email address: " . $email . "<br />";
 	$message .= "Message: <br />";
 	$message .= $contact_message;
-	$message .= "<br /> ----- <br /> This email was sent from your site's contact form. <br />";
+	$message .= "<br /> ----- <br /> This message was sent from your site's contact form. <br />";
 
 	$url = $_ENV["BLOWERIO_URL"] . "/messages";
-	$data = array('to' => '+12069400857', 'message' => 'Hello from Blower.io');
+	$data = array('to' => '+12069400857', 'message' => $message);
 
 	$ch = curl_init($url);
 	curl_setopt($ch, CURLOPT_POST, 1);
